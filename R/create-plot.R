1bfb49699e3ad6d4eb7033ba129c25c1b89beee7
Matthew Wilson <m.wilson92@live.co.uk>
2021-10-19 11:29:32 +0100

    update title to change dynamically. resave plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,14 +9,14 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Brisbane"
+city = "Sydney"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am, color = "red")) + 
   geom_point() +
-  labs(title = "Temperature vs Humidity for Brisbane")
+  labs(title = paste0("Temperature vs Humidity for", city))
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
