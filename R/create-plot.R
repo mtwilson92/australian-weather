f784142dbb803fb1f48822599dcff48e4ccba713
mtwilson92 <92781360+mtwilson92@users.noreply.github.com>
2021-10-21 12:25:21 +0100

    Update create-plot.R

--- R/create-plot.R
+++ R/create-plot.R
@@ -14,7 +14,7 @@ city = "Sydney"
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am, color = "purple")) + 
+  ggplot(aes(x = Temp9am, y = Humidity9am, color = "green")) + 
   geom_point() +
   labs(title = paste("Temperature vs Humidity for", city)) +
   ylab("Humidity") +
